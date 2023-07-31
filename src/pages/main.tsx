10eef4df39d2ae977e6654fc31d20fe2bac1a57e
Hartaithan <hartaithan@gmail.com>
2023-07-31 15:03:10 +0900

    create users cache map

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -7,6 +7,8 @@ import { getRandomNumber } from "../helpers/number";
 
 const URL = "https://jsonplaceholder.typicode.com/users";
 
+const usersCacheMap: Map<number, IUser> = new Map();
+
 const MainPage: FC = () => {
   const [item, setItem] = useState<IUser | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -14,10 +16,16 @@ const MainPage: FC = () => {
   const receiveRandomUser = async () => {
     setError(null);
     const id = getRandomNumber(1, 10);
+    if (usersCacheMap.has(id)) {
+      const cached = usersCacheMap.get(id);
+      setItem(cached || null);
+      return;
+    }
     const response = await fetch(`${URL}/${id}`);
     const data = await response.json();
     if (response.ok) {
       const user = data as IUser;
+      usersCacheMap.set(id, user);
       setItem(user || null);
     } else {
       setItem(null);
