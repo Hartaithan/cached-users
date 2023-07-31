a1d4d2a1683b298859833fffc0e647bba75d810d
Hartaithan <hartaithan@gmail.com>
2023-07-31 13:40:09 +0900

    install prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,14 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
+    "prettier/prettier": ["error"],
+    "no-console": ["error", { allow: ["info", "error"] }],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
